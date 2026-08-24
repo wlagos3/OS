e647a2d3fb22042a43a610845fad0ce7860d00a9
William Lagos <wlagos@patterndata.ai>
2026-08-23 23:40:34 -0400

    -

--- src/kernel.c
+++ src/kernel.c
@@ -4,10 +4,15 @@
 #include "drivers/virtual/fwcfg.h"
 #include <stdint.h>
 
+
 #include "gfx.h"
+
+extern const int32_t bar;
+extern uint32_t add(uint32_t);
 
 void kernel_main(void) {
     kprintf("Hello Computer\n");
+    kprintf("bar from hello.k1 = %d\n", bar);
 
     fw_config_init();
     uint16_t key; uint32_t size;
@@ -18,5 +23,10 @@ void kernel_main(void) {
 
     ramfb_init();
     gfx_fill_rect(0, 0, 740, 480, 0x1010FF);
+
+    uint32_t ten = 10;
+    uint32_t new_num = add(ten);
+    kprintf("num: %d\n", new_num);
+
     for (;;);
 }
--- src/lib/mem.c
+++ src/lib/mem.c
@@ -5,9 +5,11 @@
 #include "mem.h"
 
 #include <stdint.h>
+#include "printf.h"
 
 
 void *memcpy(void *dst, const void *src, size_t n) {
+    kprintf("calling memcpy\n");
     uint8_t *d = dst;
     const uint8_t *s = src;
     for (size_t i = 0; i < n; i++) *d++ = *s++;
@@ -15,12 +17,14 @@ void *memcpy(void *dst, const void *src, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
+    kprintf("calling memset\n");
     uint8_t *d = s;
     for (size_t i = 0; i < n; i++)
         *d++ = c;
     return s;
 }
 void *memmove(void *dst, const void *src, size_t n) {
+    kprintf("yo");
     if (dst < src)  return memcpy(dst, src, n);
     if (dst == src) return dst;
     uint8_t *d = dst;
@@ -30,6 +34,7 @@ void *memmove(void *dst, const void *src, size_t n) {
 }
 
 int memcmp(const void *a, const void *b, size_t n) {
+    kprintf("yo");
     const uint8_t *d = a;
     const uint8_t *s = b;
     for (size_t i = 0; i < n; i++) {
